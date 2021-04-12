d504311e63d3864452c39f16307da27ac3e9e8ea
Mohd-Affan <affan.taj@gmail.com>
2021-04-13 00:01:08 +0530

    12-04-21

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
 
-import Notifications from './src/components/Notifications';
+// import Notifications from './src/components/Notifications';
+import GoogleSignIn from './src/components/GooglSignIn';
 
-AppRegistry.registerComponent(appName, () => Notifications);
+AppRegistry.registerComponent(appName, () => GoogleSignIn);
